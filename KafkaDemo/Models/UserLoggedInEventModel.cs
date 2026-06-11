85fad8193e4def7bfaa2f729df448eba9352ab7b
ashkan semsar <ashkansemsar@hotmail.com>
2026-06-11 18:49:55 +0300

    default description

--- KafkaDemo/Models/UserLoggedInEventModel.cs
+++ KafkaDemo/Models/UserLoggedInEventModel.cs
@@ -1,3 +1,14 @@
-﻿namespace KafkaDemo.Models;
+namespace KafkaDemo.Models;
 
-public record UserLoggedInEventModel(string UserId, string UserName, DateTime LoggedInAt, string Description);
+public record UserLoggedInEventModel
+{
+    private const int DefaultDescriptionLength = 10 * 1024;
+
+    public string UserId { get; init; } = string.Empty;
+
+    public string UserName { get; init; } = string.Empty;
+
+    public DateTime LoggedInAt { get; init; }
+
+    public string Description { get; init; } = new('A', DefaultDescriptionLength);
+}
